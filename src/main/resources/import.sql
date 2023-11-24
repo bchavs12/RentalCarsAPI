c272c7cafc026802a4c24424b976d4efe1d11599
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-11-24 19:37:20 -0300

    feat: webConfig CORS to port:3000

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,8 @@
 -- CATEGORIAS
-INSERT INTO TB_CATEGORIES (name, description, image_url) VALUES ('Sedan', 'Veículos confortáveis e espaçosos, ideais para famílias e viagens longas.', 'url_imagem_sedan');
-INSERT INTO TB_CATEGORIES (name, description, image_url) VALUES ('SUV', 'Veículos robustos com alta capacidade de carga e desempenho em diferentes terrenos.', 'url_imagem_suv');
-INSERT INTO TB_CATEGORIES (name, description, image_url) VALUES ('Compacto', 'Carros pequenos e econômicos, perfeitos para uso urbano e fácil estacionamento.', 'url_imagem_compacto');
+INSERT INTO TB_CATEGORIES (name, description, image_url) VALUES ('Sedan', 'Veículos confortáveis e espaçosos, ideais para famílias e viagens longas.', 'https://i.imgur.com/Pf1G4Fj.jpg');
+INSERT INTO TB_CATEGORIES (name, description, image_url) VALUES ('SUV', 'Veículos robustos com alta capacidade de carga e desempenho em diferentes terrenos.', 'https://i.imgur.com/e3ruOvT.jpg');
+INSERT INTO TB_CATEGORIES (name, description, image_url) VALUES ('Compacto', 'Carros pequenos e econômicos, perfeitos para uso urbano e fácil estacionamento.', 'https://i.imgur.com/9hlFRAj.jpg');
+INSERT INTO TB_CATEGORIES (name, description, image_url) VALUES ('Elétrico', 'Carros elétricos com alta eficiência energética, ideais para a sustentabilidade.', 'https://i.imgur.com/DzUAL3F.jpg');
 
 -- CIDADES
 INSERT INTO TB_CITIES (name, state) VALUES ('São Paulo', 'SP');
@@ -14,12 +15,12 @@ INSERT INTO TB_RENTAL_COMPANIES (name, city) VALUES ('RentNow MG', 2);
 INSERT INTO TB_RENTAL_COMPANIES (name, city) VALUES ('CarRent GO', 3);
 
 -- CARROS
-INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Toyota', 'Corolla', 2020, 'url_imagem_corolla', 150, true, 1, 1);
-INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Ford', 'Fiesta', 2019, 'url_imagem_fiesta', 120, true, 2, 2);
-INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Nissan', 'Versa', 2021, 'url_imagem_versa', 130, true, 1, 1);
-INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Fiat', 'Argo', 2022, 'url_imagem_argo', 110, true, 3, 2);
-INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Renault', 'Sandero', 2018, 'url_imagem_sandero', 100, true, 3, 3);
-INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Volkswagen', 'Polo', 2020, 'url_imagem_polo', 140, true, 2, 1);
+INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Toyota', 'Corolla', 2020, 'https://i.imgur.com/odvG3r1.jpg', 150, true, 1, 1);
+INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Ford', 'Fiesta', 2019, 'https://i.imgur.com/2WBVX0M.jpg', 120, true, 2, 2);
+INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Nissan', 'Versa', 2021, 'https://i.imgur.com/9ELA36t.jpg', 130, true, 1, 1);
+INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Fiat', 'Argo', 2022, 'https://i.imgur.com/k9sTxTZ.jpg', 110, true, 3, 2);
+INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Renault', 'Sandero', 2018, 'https://i.imgur.com/xmq75NO.jpg', 100, true, 3, 3);
+INSERT INTO TB_CARS (brand, model, car_year, image_url, price_per_day, is_available, fk_category_id, fk_rental_company_id) VALUES ('Volkswagen', 'Polo', 2020, 'https://i.imgur.com/3Yh0WMu.jpg', 140, true, 2, 1);
 
 -- CARACTERISTICA
 INSERT INTO TB_CHARACTERISTICS (seats, trunk, icon, car_doors, fk_car_id) VALUES ('4 lugares', 'Porta-malas espaçoso', 'icon_url', 4, 1);
