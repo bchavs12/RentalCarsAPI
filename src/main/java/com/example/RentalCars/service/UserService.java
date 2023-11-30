1e080163e634cd372aafaad2d9fc794c96fa9510
Carlos-Landerdahl <carloslanderdahl27@gmail.com>
2023-11-30 00:04:02 -0300

    feat: Get user using token

--- src/main/java/com/example/RentalCars/service/UserService.java
+++ src/main/java/com/example/RentalCars/service/UserService.java
@@ -1,14 +1,19 @@
 package com.example.RentalCars.service;
 
+import com.example.RentalCars.dto.response.UserResponseDTO;
 import com.example.RentalCars.model.Role;
 import com.example.RentalCars.model.User;
 import com.example.RentalCars.projections.UserDetailsProjection;
 import com.example.RentalCars.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -33,5 +38,24 @@ public class UserService implements UserDetailsService {
 		}
 		
 		return user;
+	}
+
+	protected User authenticated() {
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+			String username = jwtPrincipal.getClaim("username");
+
+			return repository.findByEmail(username).get();
+		}
+		catch (Exception e) {
+			throw new UsernameNotFoundException("Email not found");
+		}
+	}
+
+	@Transactional(readOnly = true)
+	public UserResponseDTO getMe() {
+		User user = authenticated();
+		return new UserResponseDTO(user);
 	}
 }
